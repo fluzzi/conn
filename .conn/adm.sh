4f3e6ec6a7e32b629dec109e7f1a949955624dd2
fluzzi <fluzzi@gmail.com>
2019-03-01 10:48:31 -0300

    simplify adm

--- .conn/adm.sh
+++ .conn/adm.sh
@@ -1,8 +1,5 @@
 adm(){
-  if [ $1 = "add" ] && [ $# -eq 1 ]; then echo adding!; exit 1; fi
   if [ $1 = "add" ] && [ $# -eq 2 ]; then echo adding $2!; exit 1; fi
-  if [ $1 = "del" ] && [ $# -eq 1 ]; then echo deleting!; exit 1; fi
   if [ $1 = "del" ] && [ $# -eq 2 ]; then echo deleting $2!; exit 1; fi
-  if [ $1 = "mod" ] && [ $# -eq 1 ]; then echo modifying!; exit 1; fi
   if [ $1 = "mod" ] && [ $# -eq 2 ]; then echo modifying $2!; exit 1; fi
 }
